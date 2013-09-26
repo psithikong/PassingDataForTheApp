8235674e70138d19abbbeea43d08520e224cd0ae
psithikong <paeng@wl-10-190-210-85.wireless.duke.edu>
2013-09-26 14:14:19 -0400

    Initial Commit

---
+++ PassingDataForTheApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PassingDataForTheApp
+//
+//  Created by paeng on 9/26/13.
+//  Copyright (c) 2013 paeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PassingDataForTheApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PassingDataForTheApp
+//
+//  Created by paeng on 9/26/13.
+//  Copyright (c) 2013 paeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
